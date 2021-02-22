9e91bb8b945c024eea055458273ab641deb4d722
Jan Schwoerer <tmpexcleml@gmail.com>
2021-02-22 22:35:20 +0100

    Updates BuildSearchResult to use SearchResult

--- src/main/groovy/com/absolutegalaber/buildz/domain/BuildSearchResult.groovy
+++ src/main/groovy/com/absolutegalaber/buildz/domain/BuildSearchResult.groovy
@@ -1,30 +1,41 @@
 package com.absolutegalaber.buildz.domain
 
 import com.absolutegalaber.buildz.api.model.IBuild
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 import org.springframework.data.domain.Page
 
-@ToString(includes = ['builds', 'page', 'totalElements', 'totalPages', 'hasNext', 'hasPrevious'])
-@EqualsAndHashCode(includes = ['builds', 'page', 'totalElements', 'totalPages', 'hasNext', 'hasPrevious'])
-class BuildSearchResult implements Serializable {
-    List<IBuild> builds = []
-    Integer page
-    Long totalElements
-    Integer totalPages
-    Boolean hasNext
-    Boolean hasPrevious
+@ToString(includeSuperProperties = true)
+@EqualsAndHashCode(callSuper = true)
+final class BuildSearchResult extends SearchResult<IBuild> {
 
+    /**
+     * Default Constructor needed for JSON generating libraries
+     */
+    BuildSearchResult() {}
+
+    private BuildSearchResult(SearchResult sr) {
+        super(sr.results, sr.page, sr.totalElements, sr.totalPages, sr.hasNext, sr.hasPrevious)
+    }
+
+    /**
+     * Generate a BuildSearchResult based on a query Page.
+     *
+     * @param result A page of a query
+     * @return the BuildSearchResult based on the query Page
+     */
     static BuildSearchResult fromPageResult(Page<Build> result) {
-        BuildSearchResult toReturn = new BuildSearchResult()
-        toReturn.getBuilds().addAll(result.getContent().collect {
-            IBuild.of(it)
-        })
-        toReturn.setPage(result.getNumber())
-        toReturn.setTotalPages(result.getTotalPages())
-        toReturn.setTotalElements(result.getTotalElements())
-        toReturn.setHasNext(result.hasNext())
-        toReturn.setHasPrevious(result.hasPrevious())
-        toReturn
+        new BuildSearchResult(fromPageResult(result, IBuild.&of))
+    }
+
+    @JsonIgnore
+    List<IBuild> getResults() {
+        return super.results
+    }
+
+    List<IBuild> getBuilds() {
+        return super.results
     }
 }
