45755ba75e8e804e1c178654285dc45b0a7ea962
Jan Schwoerer <janlucasschwoerer@gmail.com>
2021-02-27 14:32:46 +0100

    Updates BuildSearch to use BaseSearch

--- src/main/groovy/com/absolutegalaber/buildz/domain/BuildSearch.groovy
+++ src/main/groovy/com/absolutegalaber/buildz/domain/BuildSearch.groovy
@@ -2,29 +2,17 @@ package com.absolutegalaber.buildz.domain
 
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
-import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 
 @ToString(includes = ['project', 'branch', 'minBuildNumber', 'maxBuildNumber', 'labels'])
 @EqualsAndHashCode(includes = ['project', 'branch', 'minBuildNumber', 'maxBuildNumber', 'labels'])
-class BuildSearch implements Serializable {
+class BuildSearch extends BaseSearch {
+    private static final DEFAULT_SORT_ATTRIBUTE = 'buildNumber'
     String project
     String branch
     Long minBuildNumber
     Long maxBuildNumber
     Map<String, String> labels = [:]
-    Integer pageSize
-    Integer page
-    String sortAttribute
-    String sortDirection
-
-    PageRequest page() {
-        int thePage = page ?: 0
-        int thePageSize = pageSize ?: 10
-        Sort.Direction theDirection = sortDirection ? Sort.Direction.fromString(sortDirection) : Sort.Direction.DESC
-        String theSortAttribute = sortAttribute ?: "buildNumber"
-        PageRequest.of(thePage, thePageSize, Sort.by(theDirection, theSortAttribute))
-    }
 
     static BuildSearch fromArtifact(Artifact artifact) {
         new BuildSearch(
@@ -33,8 +21,12 @@ class BuildSearch implements Serializable {
                 labels: artifact.labels,
                 pageSize: 1,
                 page: 0,
-                sortAttribute: 'buildNumber',
+                sortAttribute: DEFAULT_SORT_ATTRIBUTE,
                 sortDirection: Sort.Direction.DESC.name()
         )
+    }
+
+    protected String defaultSortAttribute() {
+        return DEFAULT_SORT_ATTRIBUTE
     }
 }
